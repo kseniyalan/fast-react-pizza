48cc04406e4e88554db200f8a4f60def927ebfac
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-04-27 22:38:04 +0200

    Initial commit

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,18 @@
+import { formatCurrency } from "../../utils";
+
+function CartItem({ item }) {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
