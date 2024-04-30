ee30028de14bd481f505853e354af022d6bda936
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-04-30 23:38:55 +0200

    Error handling

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
@@ -31,7 +31,17 @@ const fakeCart = [
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
+  const navigation = useNavigation();
+
+  // Get the submittin state from navigation - when the form is submitting, the navigation state will be "submitting"
+  const isSubmitting = navigation.state === "submitting";
   const cart = fakeCart;
+
+  //Check for errors
+  //This component is connected with the router and the action function
+  //If we return an object with errors from the router action function, we can get them here
+
+  const formErrors = useActionData();
 
   return (
     <div>
@@ -51,6 +61,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -73,7 +84,12 @@ function CreateOrder() {
         {/* Hidden input to provide cart data into the action function */}
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
-          <button>Order now</button>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'Placing order...' : 'Order now!'}
+          </button>
         </div>
       </Form>
     </div>
@@ -90,6 +106,15 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
+  //Validation
+  const errors = {};
+  if (!isValidPhone(order.phone)) {
+    errors.phone = "Please enter a valid phone number, we might need to call you!";
+  }
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is ok, we can create the order and redirect to the order page
   const newOrder = await createOrder(order);
 
   //Lets redirect to the order page with the new order id
