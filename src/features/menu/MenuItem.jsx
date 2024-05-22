80dd59ca8fc17f338727d8197f811ec8ec1ceae8
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-05-22 23:57:50 +0200

    Menu styles

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,17 +1,28 @@
 import { formatCurrency } from '../../utils/helpers';
+import Button from '../../ui/Button';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className='flex gap-4 py-2'>
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+      />
+      <div className='flex flex-col'>
+        <p className='font-medium'>{name}</p>
+        <p className='text-sm italic text-stone-500 capitalize'>{ingredients.join(', ')}</p>
+        {/* Price and button */}
+        <div className='flex items-center justify-between mt-auto'>
+          {!soldOut ? (
+            <p className='text-sm'>{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className='text-sm uppercase font-medium text-stone-500'>Sold out</p>
+          )}
         </div>
+        <Button>Add to cart</Button>
       </div>
     </li>
   );
