878a544419faa797491e959cec5bf4827c2a4495
Ksenia Lanetskaia <kseniia.lanetskaia@scr.sk>
2024-05-28 21:31:38 +0200

    Cart styles

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -29,16 +30,21 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className='px-4 py-3'>
       <LinkButton to="/menu">
         &larr; Back to menu
       </LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className='mt-7 text-xl font-semibold'>Your cart, %NAME%</h2>
+      <ul className='divide-y divide-stone-200 border-b mt-4'>
+        {cart.map((item) => (
+          <CartItem key={item.pizzaId} item={item} />
+        ))}
+      </ul>
 
-      <div>
+      <div className='mt-6 space-x-2'>
         <Button size="primary" to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+        <Button size="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, size }) {
 
-  const base = "bg-yellow-400 uppercase font-semibold text-stone-800 tracking-wide inline-block rounded-fullhover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+  const base = "bg-yellow-400 uppercase font-semibold text-stone-800 tracking-wide inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary: "uppercase font-semibold text-stone-400 tracking-wide inline-block rounded-full border-2 border-stone-300 transition-colors duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring-2 focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
 
   };
 
